152424675ee3f8f8c5663836fe25190d58c9b763
Shea Leffler <shea@errno.com>
2021-12-02 09:55:03 -0800

    Update hv-ecs

--- hv/src/ecs.rs
+++ hv/src/ecs.rs
@@ -1,7 +1,7 @@
 //! Re-export of the `hecs` ECS, with some extra goodies.
 
-pub use ::ecs::*;
-pub use ::ecs_derive::*;
-pub use ::elastic::external::ecs::*;
-pub use ::lua::hv::ecs::*;
-pub use ::yaks::*;
+pub use ecs::*;
+pub use ecs_derive::*;
+pub use elastic::external::ecs::*;
+pub use lua::hv::ecs::*;
+pub use yaks::*;
