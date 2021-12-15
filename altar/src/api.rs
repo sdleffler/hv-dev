c63d0d3b5ead6fdae64f68ef31bd491b352ddf00
Shea Leffler <shea@errno.com>
2021-12-15 11:57:23 -0800

    hook types

--- altar/src/api.rs
+++ altar/src/api.rs
@@ -7,11 +7,12 @@ use parry3d::shape::SharedShape;
 lazy_static::lazy_static! {
     pub static ref ALTAR: Module = Module::new("altar", "altar", altar_module);
     pub static ref PHYSICS: Module = Module::new("physics", "altar.physics", physics_module);
+    pub static ref TYPES: Module = Module::new("types", "altar.types", types_module);
 }
 
 fn altar_module(lua: &Lua) -> Result<ModuleBuilder> {
     let mut builder = ModuleBuilder::new(lua)?;
-    builder.submodule(&*PHYSICS)?;
+    builder.submodule(&*PHYSICS)?.submodule(&*TYPES)?;
 
     Ok(builder)
 }
@@ -28,6 +29,18 @@ fn physics_module(lua: &Lua) -> Result<ModuleBuilder> {
         .userdata_type::<CcdEnabled>("CcdEnabled")?
         .userdata_type::<KinematicMarker>("KinematicMarker")?
         .userdata_type::<SharedShape>("Shape")?;
+
+    Ok(builder)
+}
+
+fn types_module(lua: &Lua) -> Result<ModuleBuilder> {
+    use crate::types::*;
+    let mut builder = ModuleBuilder::new(lua)?;
+    builder
+        .userdata_type::<PreTickHook>("PreTickHook")?
+        .userdata_type::<UpdateHook>("UpdateHook")?
+        .userdata_type::<DrawHook>("DrawHook")?
+        .userdata_type::<PostTickHook>("PostTickHook")?;
 
     Ok(builder)
 }
--- altar/src/types.rs
+++ altar/src/types.rs
@@ -1,3 +1,5 @@
+use hv::prelude::*;
+
 pub type Float = f32;
 
 #[derive(Debug, Clone, Copy)]
@@ -14,3 +16,71 @@ pub struct RemainingUpdateDt(pub f32);
 
 #[derive(Debug, Clone, Copy)]
 pub struct UpdateTick(pub u64);
+
+#[derive(Debug, Clone, Copy)]
+pub struct PreTickHook;
+
+#[derive(Debug, Clone, Copy)]
+pub struct UpdateHook;
+
+#[derive(Debug, Clone, Copy)]
+pub struct DrawHook;
+
+#[derive(Debug, Clone, Copy)]
+pub struct PostTickHook;
+
+impl LuaUserData for PreTickHook {
+    fn on_metatable_init(table: Type<Self>) {
+        table.mark_component().add_clone().add_copy();
+    }
+
+    fn on_type_metatable_init(table: Type<Type<Self>>) {
+        table.mark_component_type();
+    }
+
+    fn add_type_methods<'lua, M: LuaUserDataMethods<'lua, Type<Self>>>(methods: &mut M) {
+        methods.add_function("new", |_, ()| Ok(Self));
+    }
+}
+
+impl LuaUserData for UpdateHook {
+    fn on_metatable_init(table: Type<Self>) {
+        table.mark_component().add_clone().add_copy();
+    }
+
+    fn on_type_metatable_init(table: Type<Type<Self>>) {
+        table.mark_component_type();
+    }
+
+    fn add_type_methods<'lua, M: LuaUserDataMethods<'lua, Type<Self>>>(methods: &mut M) {
+        methods.add_function("new", |_, ()| Ok(Self));
+    }
+}
+
+impl LuaUserData for DrawHook {
+    fn on_metatable_init(table: Type<Self>) {
+        table.mark_component().add_clone().add_copy();
+    }
+
+    fn on_type_metatable_init(table: Type<Type<Self>>) {
+        table.mark_component_type();
+    }
+
+    fn add_type_methods<'lua, M: LuaUserDataMethods<'lua, Type<Self>>>(methods: &mut M) {
+        methods.add_function("new", |_, ()| Ok(Self));
+    }
+}
+
+impl LuaUserData for PostTickHook {
+    fn on_metatable_init(table: Type<Self>) {
+        table.mark_component().add_clone().add_copy();
+    }
+
+    fn on_type_metatable_init(table: Type<Type<Self>>) {
+        table.mark_component_type();
+    }
+
+    fn add_type_methods<'lua, M: LuaUserDataMethods<'lua, Type<Self>>>(methods: &mut M) {
+        methods.add_function("new", |_, ()| Ok(Self));
+    }
+}
